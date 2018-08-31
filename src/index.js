e296276caf83cd24b76b37fb93f22c5826342795
RumenRT <33309rt@gmail.com>
2018-08-31 09:28:53 -0400

    react routes done

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Happy from './components/Happy';
+import Sleepy from './components/Sleepy';
+import Guilty from './components/Guilty';
+
+// your code goes here
+class Home extends Component{
+
+  getKittens(event) {
+    event.preventDefault();
+  }
+  render(){
+    return(
+      <BrowserRouter>
+          <div>
+            <nav>
+              <ul><li><Link to="/">Home</Link></li>
+                <li><Link to="/happy">Happy</Link></li>
+                <li><Link to="/guilty">Guilty</Link></li>
+                <li><Link to="/sleepy">Sleepy</Link></li>
+              </ul>
+            </nav>
+            <Route path="/happy" component={Happy} />
+            <Route path="/guilty" component={Guilty} />
+            <Route path="/sleepy" component={Sleepy} />
+          </div>
+      </BrowserRouter>
+    )
+  }}
+ReactDOM.render(<Home />,document.getElementById('root'));
